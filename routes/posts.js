8633373c9e0002a6942e1e6c2ea449e6683f9f02
Sujay Kadam <sujay.kadam17@siesgst.ac.in>
2022-01-05 13:31:38 +0530

    Initial commit

---
+++ routes/posts.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+const Post = require('../models/post')
+
+// Getting all
+router.get('/all', async (req, res) => {
+  try {
+    const posts = await Post.find().sort({'_id':-1})
+    res.json(posts)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// Getting by limit and offset
+router.get('/', async (req, res) => {
+  //skip is a variable maintained at frontend where the number of already returned posts is stored
+  skip = req.query.skip
+  //limit is a variable maintained at frontend and stores number of posts that are to be returned
+  limit = req.query.limit
+  try {
+    posts = await Post.find().skip(skip).limit(limit).sort({'_id':-1})
+    if (posts == null){
+      return  res.status(404).json({ message: 'Cannot find post' })
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+  res.json(posts)
+
+})
+
+// Creating one
+router.post('/createpost', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  })
+  try {
+    const newPost = await post.save()
+    res.status(201).json(`Post with title ${newPost.  title} created`)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  } 
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const postsRouter = require('./routes/posts')
+
+mongoose.connect("mongodb+srv://sujay:garudwar@cluster0.8dehr.mongodb.net/yocketPosts?retryWrites=true&w=majority", { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+
+app.use('/posts', postsRouter)
+
+app.get('/', (req, res) => {
+    res.json({
+      "endpoints":{
+        "posts/all":{
+            "method":"get",
+            "returns":"all the posts in database"
+        },
+        "posts/":{
+            "method":"get",
+            "req.query":{
+                "skip":{
+                    "type":"number",
+                    "purpose":"identify from where to start slicing the data to avoid duplication"
+                },
+                "limit":{
+                    "type":"number",
+                    "purpose":"identify number of posts to be returned"
+                }
+            }
+        },
+        "posts/createPost":{
+            "method":"post","req.body":{
+                "title":"string",
+                "content":"string"
+            }
+        }
+      }
+    })
+  })
+
+app.listen(3000, () => console.log('Server Started'))
